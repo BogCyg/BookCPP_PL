e9cdef52fc2abb72b8726f8666c1db7d4cf2cea8
kapustkak <kapustka.k@gmail.com>
2022-06-27 16:16:34 +0200

    Update main.cpp
    Komentarze z listingu 3.27

--- QuadEq/src/main.cpp
+++ QuadEq/src/main.cpp
@@ -18,16 +18,16 @@
 #include "QuadEq.h"
 
 
-// With these we can simply write cout
-// instead of std::cout, etc.
+// do pisania cout,
+// a nie std::cout.
 using std::cout, std::cerr, std::cin, std::endl;
 
 
 
-// In C++ there are overloaded main functions
+// W C++ istnieją przeciążone funkcje main
 int main( void )
 {
-	// Text corresponding to TQuadEq::EEquationType 
+	// Tekst odpowiadający wartościom TQuadEq::EEquationType 
 	const std::string eq_types_text[] = {	"No real roots", 
 											"One real root", 
 											"Two real roots",
@@ -40,28 +40,28 @@ int main( void )
 	double a = 0.0, b = 0.0, c = 0.0;
 	cin >> a >> b >> c;
 
-	TQuadEq		eq( a, b, c );		// Create an initialized  object
+	TQuadEq		eq( a, b, c );		// Utwórz zainicjalizowany obiekt
 
-	// For the roots
-	double x1 {}, x2 {};			// {} to initialize vars to zero
+	// Dla pierwiastków
+	double x1 {}, x2 {};			// {} do zainicjalizowania x1 i x2 wartością zero
 
 	TQuadEq::EEqType	eq_type {};
 
 	try
 	{
-		eq_type = eq.GetRoots( x1, x2 );	// This can throw
+		eq_type = eq.GetRoots( x1, x2 );	// Tu może nastąpić zgłoszenie wyjątku
 	}
 	catch( const std::exception & e )
 	{
-		cerr << e.what() << endl;	// print the reason
-		return -1;	// Exit with a failure code
+		cerr << e.what() << endl;	// Jeśli tutaj, to błąd, więc wypisz powód
+		return -1;	// Zakończ z kodem błędu
 	}
 
 
-	// Everything ok, so print results
+	// Wszystko w porządku, więc wypisz rezultaty
 	cout << eq_types_text[ static_cast< int >( eq_type ) ];
 	
-	// In addition, if there are roots, then print them out.
+	// odatkowo, jeśli istnieją pierwiastki, wypisz je
 	switch( eq_type )
 	{
 		case TQuadEq::EEqType::kOne:
@@ -77,5 +77,5 @@ int main( void )
 			break;
 	}
 
-	return 0;	// Exit wit ok code
+	return 0;	// Zakończ z kodem OK
 }
