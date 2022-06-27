cf76ddf43a68e98db0c746f8b662591a53020e24
kapustkak <kapustka.k@gmail.com>
2022-06-27 22:52:08 +0200

    Update struct_test.c
    Komentarze z listingu A.2, A.3

--- CPro/src/struct_test.c
+++ CPro/src/struct_test.c
@@ -19,20 +19,20 @@
 
 
 
-/* Simple struct to store coeffs of the quadratic equation */
+/* Prosta struktura do przechowywania współczynników równania kwadratowego */
 struct QE
 {
 	float a, b, c;
 };
 
-/* struct QE is a tag and we need to introduce the type QE */
-/* The name of a struct is a tag, rather than the type. */
+/* struktura QE jest znacznikiem, a my musimy wprowadzić typ QE */
+/* Nazwa struktury jest znacznikiem zamiast typem. */
 typedef		struct QE			QE;
 
 
 
 
-/* Initialize QE to 0 */
+/* Zainicjalizuj QE wartością 0 */
 void Init_0( QE * qe )
 {
 	qe->a = 0.0;
@@ -40,7 +40,7 @@ void Init_0( QE * qe )
 	qe->c = 0.0;
 }
 
-/* Initialize QE to values */
+/* Zainicjalizuj QE wartościami */
 void Init( QE * qe, float a, float b, float c )
 {
 	qe->a = a;
@@ -51,7 +51,7 @@ void Init( QE * qe, float a, float b, float c )
 
 
 
-/* Computes a discriminant of the quadratic equation */
+/* Oblicza wyróżnik równania kwadratowego */
 float ComputeDelta( QE * qe )
 {
 	return qe->b * qe->b - 4.0 * qe->a * qe->c;
@@ -59,32 +59,32 @@ float ComputeDelta( QE * qe )
 
 
 
-/* Function to test the behavior of QE */
+/* Funkcja do testowania zachowania struktury QE */
 void Test_QE( void )
 {
-	/* These objects are not initialized */
+	/* Obiekty te nie są inicjalizowane */
 	QE		eq_1;
 	QE		eq_2;
 
-	/* Initialize eq_1 to 0 */
+	/* Zainicjalizuj eq_1 wartością 0 */
 	Init_0( & eq_1 );
 	Init_0( & eq_2 );
 
 
 	float a = 0.0, b = 0.0, c = 0.0;
 
-	/* Read 3 params from the keyboard. scanf accepts pointers, so & is used */
+	/* Wczytaj 3 parametry z klawiatury. scanf akceptuje wskaźniki, więc używany jest & */
 	scanf( "%f%f%f", & a, & b, & c );
 
-	/* Directly set data members of eq_2 */
+	/* Bezpośrednio ustaw dane składowe eq_2 */
 	eq_2.a = a;
 	eq_2.b = b;
 	eq_2.c = c;
 
-	/* Compute a discriminant */
+	/* Oblicz wyróżnik */
 	float delta = ComputeDelta( & eq_2 );
 
-	/* Print delta on the screen */
+	/* Wypisz deltę na ekranie */
 	printf( "delta=%lf\n", delta );
 
 }
