bdc02421c185b226defc4298ad33ff68ffeb6283
kapustkak <kapustka.k@gmail.com>
2022-06-27 17:38:33 +0200

    Update TheCube.h
    Komentarze z tabeli 4.3

--- TheCube/include/TheCube.h
+++ TheCube/include/TheCube.h
@@ -20,7 +20,7 @@
 
 
 
-using std::cout, std::cin, std::endl;	// Name aliases
+using std::cout, std::cin, std::endl;	// Aliasy nazw
 
 
 
@@ -32,98 +32,98 @@ public:
 	using value_type = double;
 	using size_type = /*int*/std::size_t;
 
-	static const size_type kDims { 3 };		// the same for all objects of this class
+	static const size_type kDims { 3 };		// to samo dla wszystkich obiektów klasy
 
-	enum EDims { kx, ky, kz };				// shortcuts for 3 dimensions
+	enum EDims { kx, ky, kz };				// skróty dla 3 wymiarów
 
 private:
 
-	// A 'smart' pointer to the 1D buffer with all data
+	// 'inteligentny' wskaźnik do 1-wymiarowego bufora ze wszystkimi danymi
 	std::unique_ptr< value_type [] >	fDataBuf;
 
-	// An array of 3 dimensions
+	// Tablica 3 wymiarów
 	std::array< size_type, kDims >		fDim;
 
 public:
 
 
-	// Default constructor
-	TheCube( void )			// empty buffer
+	// Konstruktor domyślny
+	TheCube( void )			// pusty bufor
 		:	fDataBuf( nullptr ), fDim { 0, 0, 0 }	
 	{
 	}
 
-	// Parametric constructor
+	// Konstruktor parametryczny
 	TheCube( const /*int*/size_type dx, const /*int*/size_type dy, const /*int*/size_type dz )
 		: fDim{ dx, dy, dz }
 	{
-		// Allocate a 1D array of value_type elements and assign to fDataBuf
+		// Przydziel 1-wymiarową tablicę elementów value_type i przypisz do fDataBuf
 		fDataBuf = std::make_unique< value_type [] >( fDim[ kx ] * fDim[ ky ] * fDim[ kz ] );
 	}
 
 
-	// Copy constructor
+	// Konstruktor kopiujący
 	TheCube( const TheCube & cube )
 	{
-		fDim = cube.fDim;				// First copy the dimensions
+		fDim = cube.fDim;				// Najpierw skopiuj wymiary
 
 		const auto	elems = fDim[ kx ] * fDim[ ky ] * fDim[ kz ];
 
-		// Whatever was held by fDataBuf will be first deleted.
-		// Then, a new buffer will be allocated and attached to fDataBuf
+		// Cokolwiek było w fDataBuf zostanie najpierw usunięte.
+		// Następnie zostanie zaalokowany nowy bufor i dołączony do fDataBuf
 		fDataBuf = std::make_unique< value_type [] >( elems );
 
-		// Finally, "deep" copy data to the "this" buffer
-		//                dest             source                 bytes
+		// Na koniec "głęboko" skopiuj dane do "tego" bufora
+		//               cel                źródło                 bajty
 		std::memcpy( fDataBuf.get(), cube.fDataBuf.get(), elems * sizeof( value_type ) );	
 	}
 
-	// Assignment operator
+	// Operator przypisania
 	TheCube & operator = ( const TheCube & cube )
 	{
-		// We cannot copy if the same object
+		// Nie możemy skopiować, jeśli to ten sam obiekt
 		if( this != & cube )
 		{
-			fDim = cube.fDim;					// First copy the dimensions
+			fDim = cube.fDim;					// Najpierw skopiuj wymiary
 
 			const auto	elems = fDim[ kx ] * fDim[ ky ] * fDim[ kz ];
 
-			// Whatever was held by fDataBuf will be first deleted
-			// Then new block will be allocated and assigned to fDataBuf
+			// Cokolwiek było w fDataBuf zostanie najpierw usunięte
+			// Nowy blok zostanie przydzielony i przypisany do fDataBuf
 			fDataBuf = std::make_unique< value_type [] >( elems );
 
-			// Finally, deep copy data to the "this" buffer		
-			//         dest               source                 bytes
+			// Na koniec wykonaj głęboką kopię danych do "tego" bufora		
+			//           cel               źródło                bajty
 			memcpy( fDataBuf.get(), cube.fDataBuf.get(), elems * sizeof( value_type ) );	
 		}
 		return * this;
 	}
 
 
-	// Destructor - if planning to derive, make it virtual
+	// Destruktor – w przypadku planów dziedziczenia uczyń go wirtualnym
 	~TheCube() {}
 
 
-	// Move constructor (C++11+)
+	// Konstruktor przenoszący (C++11)
 	TheCube( TheCube && cube ) noexcept
 		: fDataBuf( nullptr ), fDim { 0, 0, 0 }
 	{
-		// Swap (exchange) dimensions
+		// Zamień (wymiana) wymiary
 		std::swap( fDim, cube.fDim );
 
-		// Swap held pointers
+		// Zamień przechowywane wskaźniki
 		fDataBuf.swap( cube.fDataBuf );
 	}
 
 
 
-	// Move assignment operator (C++11+)
+	// Operator przypisania przenoszący (C++11)
 	TheCube & operator = ( TheCube && cube ) noexcept
 	{
-		// Swap all data members between: this and cube
+		// Zamień wszystkie dane składowe między this i cube
 		std::swap( fDim, cube.fDim );
 
-		// Only exchange the pointers - do not copy the buffers!
+		// Tylko zamień wskaźniki – nie kopiuj buforów!
 		fDataBuf.swap( cube.fDataBuf );		
 
 		return * this;
@@ -133,12 +133,12 @@ public:
 
 public:
 
-	// Getters/setters
+	// Gettery/settery
 
-	// Because of EDims as a parameter we cannot ask for non exisitent dimension
+	// Ponieważ EDims jako parametr, nie możemy prosić o nieistniejące wymiary
 	auto	GetDim( EDims which_one ) const { return fDim[ which_one ]; }
 
-	// Access elements by reference - bi-directional
+	// Uzyskaj dostęp do elementów przez referencję - dwukierunkowo
 	auto & Element( const int x, const int y, const int z ) const
 	{ 
 		const auto offset = ( z * fDim[ ky ] + y ) * fDim[ kx ] + x;
@@ -147,12 +147,12 @@ public:
 
 	auto	Size( void ) const { return fDim[ kx ] * fDim[ ky ] * fDim[ kz ]; }
 
-	// It is a const function but it allows for data change
+	// Jest to funkcja const, ale pozwala na zmianę danych
 	auto	GetDataBuf( void ) const { return fDataBuf.get(); }
 
 public:
 
-	// Streaming operators are functions external to this class
+	// Operatory strumieniowania są funkcjami zewnętrznymi dla tej klasy
 	friend std::ostream & operator << ( std::ostream & o,	const	TheCube & cube );
 	friend std::istream & operator >> ( std::istream & i,	TheCube & cube );
 
